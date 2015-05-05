a1a0f9fc47a0a5ed0e73d91de19f77ea384fa686
ryanco <ryangcolwell@gmail.com>
2015-05-05 07:20:02 -0500

    Adding dependencies and setup script for virtualenv.

---
+++ setup_project.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#The purpose of this script is to setup virtualenv for the project.
+#Check for pip
+echo "Checking for pip installation..."
+hash pip 2>/dev/null || { echo >&2 "pip is required to setup the virtualenv. Install pip and try again."; exit 1; }
+
+#Check for virtualenv
+echo "Checking for virtualen installation..."
+if ! hash virtualenv 2>/dev/null; then
+    echo "virtualenv not installed, installing it now..."
+    pip install virtualenv
+else
+    #if the virtualenv director exists just run the install
+    echo "Virtual environment directory not found. Creating it now..."
+    if [ ! -d "apienv" ]; then
+        virtualenv apienv
+    fi
+    echo "Installing dependencies for project now..."
+    pip install -r requirements.txt
+fi
+
+#activate the local virtualenv
+echo "Activating virtualenv for project now..."
+source apienv/bin/activate
